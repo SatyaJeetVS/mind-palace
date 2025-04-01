a1c91da4b5013c2d3872199b7f47a94b1c1eaf11
satyajeet <satyajeet1400@gmail.com>
2025-04-01 15:39:34 +0530

    Some agent progress

--- backend/test.py
+++ backend/test.py
@@ -9,11 +9,20 @@ from tools import search_tool, wiki_tool, save_tool
 import wikipedia
 load_dotenv()
 
+class Subtopic(BaseModel):
+    title: str
+    content: str
+    subtopics: list["Subtopic"] = []
+
+# This is needed for self-referencing models
+Subtopic.model_rebuild()
+
 class ResearchResponse(BaseModel):
     topic: str
     summary: str
-    sources: list[str]
-    tools_used: list[str]
+    subtopics: list[Subtopic] = []
+    sources: list[str] = []
+    tools_used: list[str] = []
     
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash-exp")
@@ -25,7 +34,10 @@ prompt = ChatPromptTemplate.from_messages(
             "system",
             """
             You are a research assistant that will help generate a research paper.
-            Answer the user query and use neccessary tools. 
+            If the query topic is regarding a skill give practical information about the topic useful for skill building and use neccessary tools. First create several sub-topics on the query topics 
+            and the total output should have atleast 1000 words. There should be atleast 2 layers of sub-topics. Output should not have any special characters that can make it difficult to JSON parse.
+            Each sub-topic should have a title and content. The content should be a summary of the topic and the sub-topics.
+            Search on the internet and wikipedia for information and summarize the information in a research paper format.
             Wrap the output in this format and provide no other text\n{format_instructions}
             """,
         ),
@@ -34,7 +46,7 @@ prompt = ChatPromptTemplate.from_messages(
         ("placeholder", "{agent_scratchpad}"),
     ]
 ).partial(format_instructions=parser.get_format_instructions())
-print(parser.get_format_instructions())
+print("parse output here: ",parser.get_format_instructions()," ////end")
 tools = [search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
